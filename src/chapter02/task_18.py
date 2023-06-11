cc96e7caee49629dfbb1c2d52a2ff98205a971b9
DonOutcast <sham1996@yandex.ru>
2023-06-11 20:03:08 +0300

    task_18

---
+++ src/chapter02/task_18.py
@@ -0,0 +1,8 @@
+fruits = ["grape", "raspberry", "apple", "banana"]
+print(sorted(fruits))
+print(sorted(fruits, reverse=True))
+print(sorted(fruits, key=len))
+print(sorted(fruits, key=len, reverse=True))
+print(fruits.sort())
+
+
